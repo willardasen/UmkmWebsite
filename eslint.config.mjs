11bdd7f6f0e1646fa29e6278a2179e3d19aaa3bf
willardasen <willard.tio@gmail.com>
2025-07-28 10:10:14 +0700

    coba nonaktif eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,15 +12,15 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   //hapus dibwhnya nanti kalo mau aktifkan lagi
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off", // kalau perlu
-       'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
-  },
+  // {
+  //   files: ["**/*.ts", "**/*.tsx"],
+  //   rules: {
+  //     "@typescript-eslint/no-explicit-any": "off",
+  //     "@typescript-eslint/no-unused-vars": "off", // kalau perlu
+  //      'react/no-unescaped-entities': 'off',
+  //     '@next/next/no-page-custom-font': 'off',
+  //   },
+  // },
 
 ];
 
