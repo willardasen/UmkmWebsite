cc5456154b372f222072da88ebf698472b7ea87f
willardasen <willard.tio@gmail.com>
2025-07-28 09:17:54 +0700

    matikan eslint sementara

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  //hapus dibwhnya nanti kalo mau aktifkan lagi
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off", // kalau perlu
+    },
+  },
+
 ];
 
 export default eslintConfig;
